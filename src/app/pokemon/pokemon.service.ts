56a63200a6f89a7a71fbf42e50b6dc523dee7636
bryce-sncf <bryce.marcol@sncf.fr>
2023-06-12 12:31:08 +0200

    with search rxjs

--- src/app/pokemon/pokemon.service.ts
+++ src/app/pokemon/pokemon.service.ts
@@ -23,8 +23,17 @@ getPokemonById(pokemonId: number): Observable<Pokemon|undefined>
 {
   return this.http.get<Pokemon>(`api/pokemons/${pokemonId}`).pipe(
     tap((response) => this.log(response)),
-    catchError((error)=>this.handleError(error, []))
+    catchError((error)=>this.handleError(error, undefined))
   );
+}
+
+searchPokemonList(term: string): Observable<Pokemon[]> {
+  if(term.length<2)
+  return of ([]);
+  return this.http.get<Pokemon[]>(`api/pokemons/?name=${term}`).pipe(
+    tap((response) => this.log(response)),
+    catchError((error)=>this.handleError(error, []))
+  )
 }
 
 addPokemon(pokemon: Pokemon): Observable<Pokemon>{
---
+++ src/app/pokemon/search-pokemon/search-pokemon.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Pokemon } from '../pokemon';
+import { Observable, Subject, debounceTime, distinctUntilChanged, map, switchMap } from 'rxjs';
+import { PokemonService } from '../pokemon.service';
+
+@Component({
+  selector: 'app-search-pokemon',
+  templateUrl: './search-pokemon.component.html',
+})
+export class SearchPokemonComponent implements OnInit {
+
+searchTerms = new Subject<string>();
+pokemons$!: Observable <Pokemon[]>;
+
+  constructor(private router: Router,
+              private pokemonService: PokemonService,
+    ){}
+
+ngOnInit() {
+  this.pokemons$ = this.searchTerms.pipe(
+  debounceTime(300),
+  distinctUntilChanged(),
+  switchMap((term) => this.pokemonService.searchPokemonList(term))
+  )
+}
+
+search(term: string) {
+ this.searchTerms.next(term);
+}
+
+goToDetail(pokemon: Pokemon) {
+const link = ['/pokemons', pokemon.id]
+this.router.navigate(link)
+
+}
+}
+
+
